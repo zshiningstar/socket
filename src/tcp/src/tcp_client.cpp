4e4fe894698b2f47b4540ad1dfe7ae36ffb4a7e5
zsx-star <16603860375@163.com>
2021-03-02 21:17:37 +0800

    update client

--- src/tcp/src/tcp_client.cpp
+++ src/tcp/src/tcp_client.cpp
@@ -10,10 +10,10 @@
 #include <fcntl.h>
 #include <sys/shm.h>
 
-#define    MYPORT     8080
-#define    BUF_SIZE   1024
+#define    MYPORT     8080   //端口号
+#define    BUF_SIZE   1024   //数据缓冲区最大长度
 
-char* SERVER_IP = "192.168.1.50";
+char* SERVER_IP = "10.203.206.10";
 int result = 0;
 
 using namespace std;
@@ -23,40 +23,52 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "client_node");
 	ros::NodeHandle n;
 	
+	char recvbuf[BUF_SIZE];
+	
 	/*
-	 *@fuc: socket()创建socket描述字
+	 *@fuc: socket()创建套节字
 	 *
 	 */
 	int socket_cli = socket(AF_INET, SOCK_STREAM, 0);
-	struct sockaddr_in addr;  //struct关键字可不写
-	memset(&addr, 0, sizeof(addr));
-	addr.sin_family      = AF_INET;
-	addr.sin_port        = htons(MYPORT);
-	addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+	if(socket_cli < 0)
+	{
+		std::cout << "socket() error\n";
+		return -1;
+	}
 	
-	std::cout << "connecting %s   %d\n",SERVER_IP,MYPORT);
-	
-	if(connect(socket_cli, (struct sockaddr*) &saddr, sizeof(addr)) < 0)
+	/*
+	 *@fuc: 服务器端IP4地址信息,struct关键字可不写
+	 *@fuc: 初始化sever地址信息   
+	 */
+	struct sockaddr_in sev_addr;  
+	memset(&sev_addr, 0, sizeof(sev_addr));
+	sev_addr.sin_family      = AF_INET;
+	sev_addr.sin_port        = htons(MYPORT);
+	sev_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+	std::cout << "connecting..." << std::endl;
+	/*
+	 *@fuc: 使用connect()函数来配置套节字,建立一个与TCP服务器的连接
+	 */
+	if(connect(socket_cli, (struct sockaddr*) &sev_addr, sizeof(sev_addr)) < 0)
 	{ 
-		std::cout << "connect error\n";
+		std::cout << "connect error" << std::endl;
 		return -1;
 	}
 	else
-		std::cout << "connected successfullly!\n";
+		std::cout << "connected successfullly!" << std::endl;
 		
-
 	while(ros::ok)
 	{
+		/*
+		 *@fuc: 使用recv()函数来接收服务器发送的消息
+		 */
 		recv(socket_cli, recvbuf, sizeof(recvbuf), 0);
-		if(recvbuf[0] == 0x66 && recvbuf[1] == 0xcc)
-		{
-			int fir_num = recvbuf[2];
-			int sec_num = recvbuf[3];
-			result = fir_num * sec_num;
-			std::cout << "the result is:" << result;
-		}
-		close(socket_cli);
-		return 0;
+		printf("server message: %s\n", recvbuf);
 	}
+	/*
+	 *@fuc: 关闭连接
+	 */
+	close(socket_cli);
+	return 0;
 }
 
